bd306c47b723e8b3d73be243e611c70a57ba706e
Pedroxs11 <pvs_store@hotmail.com>
2026-09-10 17:19:54 -0300

    Stop animal audio when activity closes

--- app/src/main/java/com/pedroxs11/bichinhosfazendinha/MainActivity.kt
+++ app/src/main/java/com/pedroxs11/bichinhosfazendinha/MainActivity.kt
@@ -5,11 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.pedroxs11.bichinhosfazendinha.ui.ProgressionWardrobeApp
 import com.pedroxs11.bichinhosfazendinha.ui.initializeAnimalAudio
+import com.pedroxs11.bichinhosfazendinha.ui.stopAnimalAudio
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initializeAnimalAudio(applicationContext)
         setContent { ProgressionWardrobeApp() }
+    }
+
+    override fun onDestroy() {
+        stopAnimalAudio()
+        super.onDestroy()
     }
 }
