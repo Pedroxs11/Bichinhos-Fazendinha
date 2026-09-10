39523f1bf0e0f97b757edec7a456db5a3a9fd19a
Pedroxs11 <pvs_store@hotmail.com>
2026-09-10 15:07:49 -0300

    Initialize animal audio playback

--- app/src/main/java/com/pedroxs11/bichinhosfazendinha/MainActivity.kt
+++ app/src/main/java/com/pedroxs11/bichinhosfazendinha/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.pedroxs11.bichinhosfazendinha.ui.ProgressionWardrobeApp
+import com.pedroxs11.bichinhosfazendinha.ui.initializeAnimalAudio
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initializeAnimalAudio(applicationContext)
         setContent { ProgressionWardrobeApp() }
     }
 }
