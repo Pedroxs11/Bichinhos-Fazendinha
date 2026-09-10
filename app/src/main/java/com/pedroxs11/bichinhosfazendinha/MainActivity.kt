5738093e59397bec4f8791eb0884aa4b4e89e4eb
Pedroxs11 <pvs_store@hotmail.com>
2026-09-09 23:24:15 -0300

    feat: launch progression game with 10-animal wardrobe

--- app/src/main/java/com/pedroxs11/bichinhosfazendinha/MainActivity.kt
+++ app/src/main/java/com/pedroxs11/bichinhosfazendinha/MainActivity.kt
@@ -3,11 +3,11 @@ package com.pedroxs11.bichinhosfazendinha
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.pedroxs11.bichinhosfazendinha.ui.WardrobeApp
+import com.pedroxs11.bichinhosfazendinha.ui.ProgressionWardrobeApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { WardrobeApp() }
+        setContent { ProgressionWardrobeApp() }
     }
 }
