cec81b7e35c6a4357534014e726a61c027209d21
Pedroxs11 <pvs_store@hotmail.com>
2026-09-12 23:56:49 -0300

    Add app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.minhafazendinha.game"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.minhafazendinha.game"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 4
+        versionName = "0.4.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
