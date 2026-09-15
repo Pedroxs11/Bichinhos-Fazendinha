acc2573fb7a804b833511b5c6182472ee24afa27
Pedroxs11 <pvs_store@hotmail.com>
2026-09-15 17:12:52 -0300

    Prepare Minha Fazendinha test build 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.minhafazendinha.game"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
     }
 
     sourceSets["main"].res.srcDir(generatedAudioRes)
