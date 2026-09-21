ab213694268b2f739f9b332d4fba8cd81f423a9b
Pedroxs11 <pvs_store@hotmail.com>
2026-09-21 20:03:17 -0300

    Add paint prototype: paintprototype/src/main/java/com/paintbynumber/prototype/MainActivity.kt

---
+++ paintprototype/src/main/java/com/paintbynumber/prototype/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.paintbynumber.prototype
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(PaintGameView(this))
+    }
+}
